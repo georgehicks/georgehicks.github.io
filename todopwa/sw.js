fdcf958eb52441e453c218d3740d00ad75800bc1
georgehicks <george.hicks@live.com>
2025-04-14 19:32:29 -0400

    Create sw.js

---
+++ todopwa/sw.js
@@ -0,0 +1,14 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('mindtree-cache').then(cache => {
+      return cache.addAll(['/', 'index.html', 'manifest.json', 'app.js']);
+    })
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
