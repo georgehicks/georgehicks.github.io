9a866eb3928a341db8f77300ed421348dfc9710f
George Hicks <georgehicks@Georges-Mac-mini.local>
2026-06-30 19:23:53 -0400

    FocusFlow: refined, subtle scrollbars
    Replace the stark default scrollbar with a thin translucent rounded one
    (scrollbar-width: thin + styled ::-webkit-scrollbar) on the timeline,
    notes, pick, and today scroll areas. Both versions; bumped SW caches.

--- FocusFlow/sw.js
+++ FocusFlow/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'focusflow-v37';
+const CACHE = 'focusflow-v38';
 const ASSETS = ['./focus-flow.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
--- FocusFlowFireBase/sw.js
+++ FocusFlowFireBase/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'focusflowfb-v20';
+const CACHE = 'focusflowfb-v21';
 const ASSETS = ['./focus-flow.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
